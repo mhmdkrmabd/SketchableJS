6c2af251352258b4b17db6941bf4b008d5e345f6
Muhammad Karam Abed El Hay <30116323+mhmdkrmabd@users.noreply.github.com>
2021-03-23 04:30:49 +0200

    Upload the library

--- README.md
+++ README.md
@@ -1,2 +1,285 @@
-# SketchableJS
- Make your canvas sketchable with less than 3.9KB library!
+<p align="center"><img src="sketchablejs.png" width="auto" height="auto"/></p>
+<p align="center">
+Make your canvas sketchable with less than 3.9KB library!
+  </p>
+
+## Installation
+
+You can download the both versions of the library (production and developement) from [here](https://github.com/mhmdkrmabd/sketchablejs/releases/).
+
+After that step, include the library by using the `script` tag:
+
+```javascript
+<script src="sketchablejs-1.0.0.min.js" charset="utf-8"></script>
+```
+
+Or, you can add this line at the very end of the library file:
+
+```javascript
+Module.exports = Sketchable;
+```
+
+Then, import the library anywhere in your code:
+
+```javascript
+const Sketchable = require('./sketchablejs-1.0.0.min.js');
+```
+
+Now you can make a sketchable canvas:
+
+```javascript
+let canvas = document.getElementById("canvas");
+var sketch = new Sketchable(canvas);
+```
+
+Piece of cake 🎂!
+
+## Constructor Parameters
+
+The constructor takes two main parameters: the first one is the `canvas` element, which is a mandatory parameter, the other one is the `options`, and it's an optional.
+
+```javascript
+var sketch = new Sketchable(Canvas, ?Options);
+```
+
+The options are:
+
+- **width**: the width of the canvas (default is the canvas width).
+
+- **height**: the height of the canvas (default is the canvas height).
+
+- **penSize**: the size of the drawing pen (minimum value is `1`, default is `2`).
+
+- **penColor**: the color of the drawing pen (HEX, RGB, and color name are acceptable, default is `#000`).
+
+- **penType**: the type of the drawing pen (acceptable values are  `round`, `butt`and `square`, default is `round`).
+
+- **backgroundColor**: the background color of the canvas (acceptable values as the penColor, default is `#fff`).
+
+```javascript
+var canvas = document.getElementById("canvas");
+let options = {
+  width: 500,
+  height: 500,
+  penSize: 2,
+  penColor: "#000",
+  penType: "round",
+  backgroundColor: "#fff"  
+};
+var sketch = new Sketchable(canvas, options);
+```
+
+## Public Methods
+
+| Name                  | Description                                                                       | Code                                            |
+| --------------------- | --------------------------------------------------------------------------------- | ----------------------------------------------- |
+| redraw                | Redraw the sketch board (canvas) draws.                                           | `redraw(?Animation, ?Speed, ?CallbackFunction)` |
+| stopRedrawAnimation   | Stop the redrawing operation with animation and redraw without it.                | `stopRedrawAnimation()`                         |
+| undo                  | Undo, delete the last draw.                                                       | `undo(?Inner)`                                  |
+| redo                  | Redo, recover the last deleted draw.                                              | `redo(?Inner)`                                  |
+| clear                 | Clear the sketch board.                                                           | `clear(?Everything)`                            |
+| returnToDefaults      | Return to the default values for the drawing pen and the sketch board background. | `returnToDefaults()`                            |
+| export                | Export the entire sketch board as JSON to string format.                          | `export()`                                      |
+| import                | Import an exported sketch board.                                                  | `import(DrawsJSON)`                             |
+| changeBackgroundColor | Change the background color of the sketch board.                                  | `changeBackgroundColor(Color)`                  |
+| penSize               | Change the drawing pen size.                                                      | `penSize(Size)`                                 |
+| penColor              | Change the drawing pen color.                                                     | `penColor(Color)`                               |
+| penType               | Change the drawing pen type                                                       | `penType(Type)`                                 |
+| listAttriubtes        | Get all the available attributes of the sketchable object.                        | `listAttriubtes()`                              |
+
+### redraw
+
+Use this method to redraw the sketch board, for example, after clearing it. You can immediately redraw or animate the redrawing process with a controlled speed and a callback function when the animation finish.
+
+```javascript
+sketch.redraw(?Animation, ?Speed, ?CallbackFunction);
+```
+
+Paramters are:
+
+- **Animation**: whether or not animate the redrawing process (default is `false`).
+
+- **Speed**: the speed of the animation between each draw (minimum value is `1`, default is `15`).
+
+- **CallbackFunction**: trigger a function when the redrawing process finish.
+
+```javascript
+// Redraw without animation and no callback function
+sketch.redraw();
+
+// Redraw with animation, and 10ms speed between each draw
+sketch.redraw(true, 10);
+
+// Redraw with animation, 10ms speed, and predefined callback function
+function f() {
+    console.log("Animation done.")
+}
+sketch.redraw(true, 10, f);
+```
+
+### stopRedrawAnimation
+
+This method will stop the redrawing animation process, at any stage, and immediately redraw the sketch board.
+
+```javascript
+// Stop the redrawing animation
+sketch.stopRedrawAnimation();
+```
+
+### undo, redo
+
+You can undo (delete) the last draw by calling the `undo` method, while `redo`, by contrast, will recover the last deleted draw.
+
+```javascript
+sketch.undo(?Inner);
+
+sketch.redo(?Inner);
+```
+
+Note that both methods take an optional parameter called `Inner`, this parameter is not for public use but inner purpose; so you don't have to (or, it's better to not) call those two methods with a given parameter.
+
+```javascript
+// Undo the last draw
+sketch.undo();
+
+// Redo the last deleted draw
+sketch.redo();
+```
+
+### clear
+
+By calling this method, you can erase all the sketch board draws.
+
+```javascript
+sketch.clear(?Everything);
+```
+
+It takes only one parameter called `Everything`; this parameter determines whether or not empty the saved draws. It's by default set to true; in that case the redraw method will not draw anything, neither the export method will export anything, but if the parameter set to false, the sketch board will be cleared, but all the draws will still saved, exported and redrawn alongside the new ones.
+
+```javascript
+// Clear the sketch board, and the saved draws
+sketch.clear();
+
+// Clear the sketch board only, keep the saved draws
+sketch.clear(false);
+```
+
+### returnToDefaults
+
+```javascript
+sketch.returnToDefaults();
+```
+
+This method will return the drawing pen attributes and the background color of the sketch board back to the default values.
+
+```javascript
+// Return pen and sketch board background to default values
+sketch.returnToDefaults();
+/*
+Pen size = 2
+Pen color = "#000
+Pen type = "round"
+Background color = "#fff"
+*/
+```
+
+### import, export
+
+You can, by using those methods, `export` the entire sketch board in a JSON to string form, and `import` it later. Note that the `export` method takes the background color into account.
+
+```javascript
+sketch.export();
+
+sketch.import(DrawsJSON);
+```
+
+```javascript
+// Export the sketch board
+sketch.export();
+
+// Import a sketch board
+var saved = '{...';
+sketch.import(saved);
+```
+
+Also, note that when importing draws, you can redraw them, animate them, add new ones, redo, undo, etc. It's a powerful technique.
+
+### changeBackgroundColor, penSize, penColor, penType
+
+By using those methods, you're able to change the background color of the sketch board, change the drawing pen size, type, and color.
+
+```javascript
+sketch.changeBackgroundColor(Color);
+
+sketch.penSize(Size);
+
+sketch.penColor(Color);
+
+sketch.penType(Type);
+```
+
+```javascript
+// Change background color
+sketch.changeBackgroundColor("white"); // #fff and rgb(255,255,255) are valid
+
+// Change the drawing pen size
+sketch.penSize(5);
+
+// Change the drawing pen color
+sketch.penColor("red"); // #ff0000 and rgb(255,0,0) are valid
+
+
+// Change the drawing pen type
+// Acceptable values are: "round", "butt" and "square"
+sketch.penType("square");
+```
+
+### listAttributes
+
+You can use this method to get the attributes of the Sketchable object; like the pen attributes values, how many saved and deleted draws, is the user sketching right now, or the drawing process with animation is running, etc.
+
+```javascript
+sketch.listAttributes();
+```
+
+The output will be like:
+
+```json
+{
+  "Dimensions": {
+    "Width": 500,
+    "Height": 500
+  },
+  "Pen": {
+    "Size": 2,
+    "Color": "#000",
+    "Type": "round"
+  },
+  "Draw": {
+    "Saved": 60,
+    "Deleted": 10
+  },
+  "Actions": {
+    "Sketching": false,
+    "Redrawing": false
+  },
+  "BackgroundColor": "#fff"
+}
+```
+
+So, if you want to know, for example, how many saved draws are there you can use the method like this:
+
+```javascript
+var attributes = sketch.listAttributes();
+attributes.Draws.Saved; // 60
+```
+
+Or you can get the value directly like this:
+
+```javascript
+sketch.listAttributes().Draws.Saved; // 60
+```
+
+### License
+
+This library is published under the [MIT Licence](/LICENSE), happy coding 🎉.
---
+++ library/development.js
@@ -0,0 +1,331 @@
+// |~> SketchableJS Library.
+// |~> Version 1.0.0 (MIT License).
+// |~> © Muhammad A. Abu Baker.
+// |~> https://github.com/mhmdkrmabd/sketchablejs
+class Sketchable {
+  constructor(Canvas, Options = "d") {
+    this.canvas = Canvas;
+    this.ctx = Canvas.getContext('2d');
+    this.defaults = [Canvas.width, Canvas.height, 2, "#000", "round", "#fff"];
+    let main = this;
+    if (Options == "d") {
+      this.width = this.defaults[0];
+      this.height = this.defaults[1];
+      this.pen = {
+        size: this.defaults[2],
+        color: this.defaults[3],
+        type: this.defaults[4],
+      };
+      this.backgroundColor = this.defaults[5];
+    } else {
+      let opt = ["width", "height", "penSize", "penColor", "penType", "backgroundColor"];
+      let givenOpt = new Array(opt.length);
+      var i = 0;
+      while (i < opt.length) {
+        let cond = " > 0";
+        if (i >= 3) {
+          cond = ".length != 0";
+        }
+        if (eval("Options." + opt[i] + " != undefined && Options." + opt[i] + cond)) {
+          eval("givenOpt[" + i + "]" + " = Options." + opt[i]);
+        } else {
+          givenOpt[i] = main.defaults[i];
+        }
+        ++i;
+      }
+      this.width = givenOpt[0];
+      this.height = givenOpt[1];
+      this.pen = {
+        size: givenOpt[2],
+        color: givenOpt[3],
+        type: givenOpt[4]
+      };
+      console.log(givenOpt);
+      this.backgroundColor = givenOpt[5];
+    }
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this._redrawing = false;
+    this._stopRedrawAnimation = false;
+    this._sketching = false;
+    this._savedDraws = [];
+    this._deletedDraws = [];
+    this._changes = true;
+    this._drawAttr = {
+      size: this.defaults[2],
+      color: this.defaults[3],
+      type: this.defaults[4],
+    }
+    this.canvas.addEventListener('mousedown', function(e) {
+      main._startingPos(e);
+    });
+    this.canvas.addEventListener('mouseup', function() {
+      main._finishingPos();
+    });
+    this.canvas.addEventListener('mousemove', function(e) {
+      main._sketch(e);
+    });
+  }
+  _startingPos(e) {
+    if (!this._redrawing) {
+      this._sketching = true;
+      this._sketch(e);
+    }
+  }
+  _finishingPos() {
+    if (!this._redrawing) {
+      this._sketching = false;
+      this._savedDraws.push("PR");
+      this.ctx.beginPath();
+    }
+  }
+  _sketch(e) {
+    if (!this._redrawing) {
+      if (!this._sketching) {
+        this.ctx.beginPath();
+        return;
+      }
+      this._draw(this.pen.size, this.pen.color, this.pen.type, e.offsetX, e.offsetY);
+    }
+  }
+  _draw(size, clr, type, x, y, save = true) {
+    this.ctx.lineWidth = size;
+    this.ctx.strokeStyle = clr;
+    this.ctx.lineCap = type;
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+    if (save) {
+      if (this._changes) {
+        let changes = "CH:" + size + "," + clr + "," + type;
+        this._savedDraws.push(changes);
+        this._changes = false;
+      }
+      let info = {
+        x: x,
+        y: y
+      };
+      this._savedDraws.push(info);
+    }
+  }
+  redraw(Animation = false, Speed = 15, CallbackFunction = "none") {
+    if (!this._redrawing) {
+      this.clear(false);
+      let main = this;
+      if (!Animation) {
+        var i = 0;
+        while (i < this._savedDraws.length) {
+          let _draw = this._savedDraws[i];
+          if (_draw == "PR") {
+            this.ctx.beginPath();
+          } else {
+            if (_draw.x == undefined && _draw.indexOf('CH:') != -1) {
+              _draw = (_draw.replace("CH:", "")).split(",");
+              this._drawAttr.size = _draw[0];
+              this._drawAttr.color = _draw[1];
+              this._drawAttr.type = _draw[2];
+            } else {
+              main._draw(this._drawAttr.size, this._drawAttr.color, this._drawAttr.type, _draw.x, _draw.y, false);
+            }
+          }
+          ++i;
+        }
+      } else {
+        if (isNaN(Speed) || Speed == null || Speed == undefined || Speed <= 0) {
+          Speed = 15;
+        }
+        this._stopRedrawAnimation = false;
+        this._redraw_animation(0, Speed, CallbackFunction);
+      }
+    }
+  }
+  _redraw_animation(_drawID, speed, callbackFunction) {
+    this._redrawing = true;
+    if (this._stopRedrawAnimation) {
+      this._stopRedrawAnimation = false;
+      this._redrawing = false;
+      this.ctx.beginPath();
+      this.redraw();
+    } else {
+      let main = this;
+      if (_drawID <= this._savedDraws.length - 1) {
+        let _draw = this._savedDraws[_drawID];
+        if (_draw == "PR") {
+          main.ctx.beginPath();
+        } else {
+          if (_draw.x == undefined && _draw.indexOf('CH:') != -1) {
+            _draw = (_draw.replace("CH:", "")).split(",");
+            this._drawAttr.size = _draw[0];
+            this._drawAttr.color = _draw[1];
+            this._drawAttr.type = _draw[2];
+          } else {
+            main._draw(this._drawAttr.size, this._drawAttr.color, this._drawAttr.type, _draw.x, _draw.y, false);
+          }
+        }
+        ++_drawID;
+        setTimeout(function() {
+          main._redraw_animation(_drawID, speed, callbackFunction);
+        }, speed);
+      } else {
+        this._redrawing = false;
+        if (callbackFunction != undefined && callbackFunction != "none") {
+          callbackFunction();
+        }
+      }
+    }
+  }
+  stopRedrawAnimation() {
+    this._stopRedrawAnimation = true;
+  }
+  clear(Everything = true) {
+    if (!this._redrawing) {
+      this.ctx.stroke();
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.fillStyle = this.backgroundColor;
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      if (Everything) {
+        this._savedDraws = [];
+        this._deletedDraws = [];
+      }
+    }
+  }
+  undo(Inner = "no") {
+    if (!this._redrawing) {
+      let main = this;
+      if (main._savedDraws.length > 0) {
+        if (Inner == "no") {
+          main._deletedDraws.push(main._savedDraws[main._savedDraws.length - 1]);
+          main._savedDraws.pop();
+          main.undo("yes");
+        } else {
+          let _draw = main._savedDraws[main._savedDraws.length - 1];
+          if (_draw == "PR") {
+            main.redraw();
+          } else {
+            main._deletedDraws.push(_draw);
+            main._savedDraws.pop();
+            main.undo("yes");
+          }
+        }
+      } else {
+        main.clear(false);
+      }
+    }
+  }
+  redo(Inner = "no") {
+    if (!this._redrawing) {
+      let main = this;
+      if (main._deletedDraws.length > 0) {
+        if (Inner == "no") {
+          main._savedDraws.push(main._deletedDraws[main._deletedDraws.length - 1]);
+          main._deletedDraws.pop();
+          main.redo("yes");
+        } else {
+          let _draw = main._deletedDraws[main._deletedDraws.length - 1];
+          main._savedDraws.push(_draw);
+          main._deletedDraws.pop();
+          if (_draw == "PR") {
+            main.redraw();
+          } else {
+            main.redo("yes");
+          }
+        }
+      }
+    }
+  }
+  returnToDefaults() {
+    this.pen = {
+      size: this.defaults[2],
+      color: this.defaults[3],
+      type: this.defaults[4],
+    };
+    this.backgroundColor = this.defaults[5];
+    this.redraw();
+  }
+  changeBackgroundColor(Color) {
+    if (!this._redrawing) {
+      this.backgroundColor = Color;
+      this.redraw();
+    }
+  }
+  export () {
+    if (!this._redrawing) {
+      let temp;
+      temp = this._savedDraws;
+      temp.push(this.backgroundColor);
+      return JSON.stringify(temp);
+    }
+  }
+  import(DrawsJSON) {
+    if (!this._redrawing) {
+      let temp;
+      try {
+        temp = JSON.parse(DrawsJSON);
+      } catch (e) {
+        return -1;
+      }
+      if (temp[temp.length - 1] == undefined || temp[temp.length - 1] == null) {
+        return -1;
+      }
+      let bgColor = temp[temp.length - 1];
+      temp = temp.slice(0, temp.length - 1);
+      this._savedDraws = temp;
+      this._deletedDraws = [];
+      this.changeBackgroundColor(bgColor);
+    }
+  }
+  penSize(Size) {
+    if (!this._redrawing) {
+      if (Size != null && Size != undefined && Size > 0 && !isNaN(Size) && Size > 0) {
+        this.pen.size = Size;
+        this._changes = true;
+      }
+    }
+  }
+  penColor(Color) {
+    if (!this._redrawing) {
+      if (Color != null && Color != undefined && Color.length > 0) {
+        this.pen.color = Color;
+        this._changes = true;
+      }
+    }
+  }
+  penType(Type) {
+    if (!this._redrawing) {
+      let types = ["butt", "round", "square"];
+      if (Type.length != 0 && types.includes(Type)) {
+        this.pen.type = Type;
+        this._changes = true;
+      }
+    }
+  }
+  listAttriubtes() {
+    let attributes = {
+      Dimensions: {
+        Width: this.width,
+        Height: this.height
+      },
+      Pen: {
+        Size: this.pen.size,
+        Color: this.pen.color,
+        Type: this.pen.type
+      },
+      Draw: {
+        Saved: this._savedDraws.length,
+        Deleted: this._deletedDraws.length
+      },
+      Actions: {
+        Sketching: this._sketching,
+        Redrawing: this._redrawing
+      },
+      BackgroundColor: this.backgroundColor
+    };
+    return attributes;
+  }
+}
+console.log('%cSketchableJS Library v1.0.0', 'background-image: linear-gradient(to right, #30399d, #732785, #911568, #9e1b4a, #9d3030); color: #F5F5F5');
+console.log('%cAuthor: Muhammad Abed El Hay Abu Baker', 'background-image: linear-gradient(to left, #30399d, #732785, #911568, #9e1b4a, #9d3030); color: #F5F5F5');
+console.log('%cGithub: github.com/mhmdkrmabd/sketchablejs', 'background-image: linear-gradient(to right, #30399d, #732785, #911568, #9e1b4a, #9d3030); color: #F5F5F5');
